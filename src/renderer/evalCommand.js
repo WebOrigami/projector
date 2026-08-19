2a2d1f5ad0104fd4a1f441f6409330915a67ba19
Jan Miksovsky <jan@miksovsky.com>
2026-08-19 13:54:53 -0700

    Tweak

--- src/renderer/evalCommand.js
+++ src/renderer/evalCommand.js
@@ -1,4 +1,5 @@
 // Expose the oriEval function so that project DebugFeatures can make the !eval
-// command available to the user. This function isn't directly by the renderer,
-// but sits in the renderer folder so it's discoverable by the debugger.
+// command available to the user. This function isn't directly called by the
+// renderer, but sits in the renderer folder so it's discoverable by the
+// debugger.
 export { oriEval as default } from "@weborigami/origami";
